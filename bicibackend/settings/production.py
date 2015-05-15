654aebd5e311bfb1dd687827e6920d0ea63d56f0
Tiago Fassoni <tiagofassoni@gmail.com>
2015-05-14 21:27:14 -0300

    Tentando consertar settings de producao

--- bicibackend/settings/production.py
+++ bicibackend/settings/production.py
@@ -1,9 +1,9 @@
-from base import *
+from bicibackend.settings.base import *
 
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['.example.com']
+#ALLOWED_HOSTS = ['.example.com']
 
 
 # Use the cached template loader so template is compiled once and read from
@@ -14,3 +14,14 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [
         'django.template.loaders.app_directories.Loader',
     ]),
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': os.getenv('DATABASE_NAME', 'biciguia'),
+        'USER': os.getenv('DATABASE_USER', 'biciguia'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'whatever'),
+        'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DATABASE_PORT', '5432'),
+    }
+}
